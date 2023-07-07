e98b216f714b08a100b3ef8cbff28c4e679f1626
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-06 23:21:17 -0300

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import "./style/reset.css";
+import "./style/style.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
