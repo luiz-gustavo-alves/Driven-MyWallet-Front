a1acb1a835ffe9e83c3f1f287b95d62354b272ce
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-08 13:47:20 -0300

    feat: Added Logout
    Feature details:
    
    (*) Removes auth from localStorage and redirects user to Login Page.
    (*) Added title to ActionButtons

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -25,6 +25,12 @@ export default function Home() {
 
   }, [auth.token, ]);
 
+  const logout = () => {
+
+    localStorage.removeItem("auth");
+    navigate("/");
+  }
+
   if (transactions === null) {
     return <h1>Carregando...</h1>
   }
@@ -33,7 +39,7 @@ export default function Home() {
     <Container>
       <UserContent>
         <Title>Olá, {auth.username}</Title>
-        <Button>
+        <Button onClick={logout} title="Sair">
           <img src="./src/assets/images/exit-door.svg"></img>
         </Button>
       </UserContent>
@@ -42,21 +48,21 @@ export default function Home() {
 
       <Footer>
         <Option>
-          <Button>
+          <Button onClick={() => navigate("/nova-transacao/entrada")} title="Nova Entrada">
             <img src="./src/assets/images/plus.svg"></img>
           </Button>
 
-          <StyledLink to="/nova-transacao/entrada">
+          <StyledLink to="/nova-transacao/entrada" title="Nova Entrada">
             <p>Nova Entrada</p>
           </StyledLink>
         </Option>
 
         <Option>
-          <Button>
+          <Button onClick={() => navigate("/nova-transacao/saida")} title="Nova Saída">
             <img src="./src/assets/images/minus.svg"></img>
           </Button>
 
-          <StyledLink to="/nova-transacao/saida">
+          <StyledLink to="/nova-transacao/saida" title="Nova Saída">
             <p>Nova Saída</p>
           </StyledLink>
         </Option>
